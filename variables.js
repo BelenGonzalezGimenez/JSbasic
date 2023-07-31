232daac9b6dd91e86d8dbcbd296b9e66c48370f1
Ana Belen Gonzalez <belen280911@fpuna.edu.py>
2023-07-31 17:33:25 -0400

    commit: ejemplos de variables con tipos de datos

--- variables.js
+++ variables.js
@@ -1,1 +1,16 @@
 //como crear variables con la palabra reservada var
+//esta palabra reservada ya casi no se usa  
+var nombre = "Oscar";
+//estados de una variable
+    //declarar y luego inicializar
+    var edad;
+    edad = 18;
+
+
+var numeros = [1,2,3,4];
+var palabras = ["Computadora", "Teclado"];
+var persona = {
+    nombre : "Diego", 
+    edad : 30
+}
+    
