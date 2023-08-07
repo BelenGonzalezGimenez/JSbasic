e4e6a30cf0179ecf1f264f7105f9ba3859396a77
Ana Belen Gonzalez <belen280911@fpuna.edu.py>
2023-08-07 18:01:25 -0400

    commit: castear

--- cohersion.js
+++ cohersion.js
@@ -3,6 +3,10 @@
     //explicita : es cuando el programador fuerza el cambio de tipo de un dato a otro tipo
 var a = 4 + "7";
 
+var b = 4 * "7";
 console.log(a);
 
 //cambia al tipo de dato inmediato mayor
+
+var a = 20;
+var c = String(a)
