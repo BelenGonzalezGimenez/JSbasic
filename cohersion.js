3d57f4491fbd46235098a586fff7367b8ec2aae0
Ana Belen Gonzalez <belen280911@fpuna.edu.py>
2023-08-07 17:56:04 -0400

    commit: cohersion

---
+++ cohersion.js
@@ -0,0 +1,6 @@
+//cohersion
+    //implicita : es cuando el lenguaje cambia el tipo de un dato a otro tipo
+    //explicita : es cuando el programador fuerza el cambio de tipo de un dato a otro tipo
+var a = 4 + "7";
+
+console.log(a);
